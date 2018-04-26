46bf323b453f71480b159c629546759ee89ec440
Someone Curious <lalabond@hotmail.com>
2018-04-25 23:50:27 -0400

    deserializing json to object model.  Working with UI, scroll view added. Need to populate rest of fields

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,47 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = new Sandwich();
+        List<String> ingredientsList = null, alsoKnownAsList = null;
+
+        try {
+            JSONObject sandwichObject = new JSONObject(json);
+
+            sandwich.setMainName(sandwichObject.getString("mainName"));
+
+            JSONArray alsoKnownAsArray = sandwichObject.getJSONArray("alsoKnownAs");
+
+            for (int x = 0; alsoKnownAsArray.length() < x; x++) {
+                alsoKnownAsList.add(alsoKnownAsArray.getString(x));
+            }
+
+            sandwich.setAlsoKnownAs(alsoKnownAsList);
+            sandwich.setDescription(sandwichObject.getString("description"));
+            sandwich.setPlaceOfOrigin(sandwichObject.getString("placeOfOrigin"));
+            sandwich.setImage(sandwichObject.getString("image"));
+
+            JSONArray ingredientsArray = sandwichObject.getJSONArray("ingredients");
+
+            for (int x = 0; ingredientsArray.length() < x; x++) {
+                ingredientsList.add(ingredientsArray.getString(x));
+            }
+
+            sandwich.setIngredients(ingredientsList);
+
+        } catch (Exception e) {
+
+            System.out.println("LALA ERROR: " + e);
+        }
+
+
+        return sandwich;
     }
 }
